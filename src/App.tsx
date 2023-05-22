4b1219d5a5bdc89610f9710bd2b30510204ea078
AdolfoContrerasCardoso <acontrerasc1500@alumno.ipn.mx>
2023-05-22 11:58:51 -0600

    22/05/2023 Subiendo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route,Routes } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Listado from './pages/Listado';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-3 mb-2 bg-dark">
+      <div className="App">
+      <Router>
+        <div>
+          <Routes>
+            <Route path='/' element={<Listado/>}/>
+          </Routes>
+        </div>
+      </Router>
+      </div>
     </div>
   );
 }
